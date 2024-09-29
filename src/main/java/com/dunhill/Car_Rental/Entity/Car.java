b1bc9909e48fad7b8c4d32cc91f764e51ebfb925
emi <emimolishti@gmail.com>
2024-09-29 15:43:38 +0200

    Created save for category.

--- src/main/java/com/dunhill/Car_Rental/Entity/Car.java
+++ src/main/java/com/dunhill/Car_Rental/Entity/Car.java
@@ -1,9 +1,6 @@
 package com.dunhill.Car_Rental.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +15,23 @@ import java.time.LocalDate;
 @Table(name = "Cars")
 public class Car {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "Brand")
     private String brand;
+    @Column(name = "Model")
     private String model;
+    @Column(name = "Body_Type")
     private String bodyType;
+    @Column(name = "Year")
     private LocalDate year;
-
+    @Column(name = "Colour")
+    private String colour;
+    @Column(name = "Mileage")
+    private long mileAge;
+    @Column(name = "Status")
+    private String status;
+    @Column(name = "Amount")
+    private long amount;
 }
