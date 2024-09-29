3cebc0e24452bfce41f448fbee9902343ccc4136
emi <emimolishti@gmail.com>
2024-09-29 13:42:50 +0200

    Created new Category Entity.

---
+++ src/main/java/com/dunhill/Car_Rental/Entity/Category.java
@@ -0,0 +1,31 @@
+package com.dunhill.Car_Rental.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name= "Categories")
+public class Category {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Description")
+    private String description;
+
+
+}
