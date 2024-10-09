a7ce3805faa0038f2d319ae70958eff84c4c9cad
tifera <38864415+tifera@users.noreply.github.com>
2024-10-09 18:55:56 +0200

    user repo test done

---
+++ src/test/java/com/dunhill/car_rental/RepositoryTest/UserRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.dunhill.car_rental.RepositoryTest;
+
+import com.dunhill.car_rental.Entity.Car;
+import com.dunhill.car_rental.Entity.User;
+import com.dunhill.car_rental.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @BeforeEach
+    void setup(){
+        user1= User.builder().id(1L).username("ferit").email("gmail").password("bro").createdAt(LocalDateTime.now()).build();
+        user2= User.builder().id(2L).username("ferit").email("gmail").password("bro").createdAt(LocalDateTime.now()).build();
+        user3= User.builder().id(3L).username("ferit").email("gmail").password("bro").createdAt(LocalDateTime.now()).build();
+    }
+
+    @Test
+    public  void testSave(){
+        User saved = userRepository.save(user1);
+
+        assertThat(saved).isNotNull();
+        assertThat(saved.getId()).isPositive();
+        assertThat(saved.getUsername()).isEqualTo("ferit");
+    }
+
+    @Test
+    public void testGetAll(){
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+
+        List<User> found = userRepository.findAll();
+        assertThat(found).isNotNull();
+    }
+
+    @Test
+    public void testGetById(){
+        userRepository.save(user1);
+        User saved = userRepository.findById(1L).get();
+        assertThat(saved).isNotNull();
+        assertThat(saved.getId()).isPositive();
+        assertThat(saved.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    public void testDelete(){
+        User saved = userRepository.save(user1);
+        userRepository.delete(saved);
+        Optional<User> found = userRepository.findById(saved.getId());
+        assertThat(found).isNotPresent();
+    }
+}
---
+++ src/test/java/com/dunhill/car_rental/ServiceTest/UserServiceTest.java
@@ -0,0 +1,66 @@
+package com.dunhill.car_rental.ServiceTest;
+
+import com.dunhill.car_rental.Dtos.CreateCarDto;
+import com.dunhill.car_rental.Dtos.CreateUserDto;
+import com.dunhill.car_rental.Dtos.ResponseUserDto;
+import com.dunhill.car_rental.Entity.Car;
+import com.dunhill.car_rental.Entity.User;
+import com.dunhill.car_rental.mapper.UserMapper;
+import com.dunhill.car_rental.repository.UserRepository;
+import com.dunhill.car_rental.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserService userService;
+
+    private User user;
+    private ResponseUserDto responseUserDto;
+    private CreateUserDto createUserDto;
+
+    @BeforeEach
+    void setup(){
+        user= User.builder().id(1L).username("ferit").email("gmail").password("bro").createdAt(LocalDateTime.now()).build();
+        responseUserDto= ResponseUserDto.builder().id(1L).username("ferit").email("gmail").password("bro").createdAt(LocalDateTime.now()).build();
+        createUserDto= CreateUserDto.builder().username("ferit").email("gmail").password("bro").createdAt(LocalDateTime.now()).build();
+    }
+
+    @Test
+    public void save(){
+        when(userMapper.mapToEntity(any(CreateUserDto.class))).thenReturn(user);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        when(userMapper.mapToDto(any(User.class))).thenReturn(responseUserDto);
+
+        userService.save(createUserDto);
+
+        verify(userRepository).save(user);
+    }
+    @Test
+    public void delete(){
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        userService.delete(user.getId());
+        verify(userRepository).delete(user);
+    }
+
+}
